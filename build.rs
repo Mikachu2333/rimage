4037b96f8a90f011e80da2c6d61683318d0cbb35
Mikachu2333 <linkchou@yandex.com>
2026-08-04 11:15:04 +0800

    ci: fix aarch64 musl libc dependency architecture
    build: revert pre version

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use winresource::{VersionInfo, WindowsResource};
 
 // This is the pre-release version number.
-const VERSION_PRE: u16 = 3;
+const VERSION_PRE: u16 = 0;
 
 fn main() {
     // only run if target os is windows
